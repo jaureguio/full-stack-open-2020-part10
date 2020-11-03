9718c180ef48e41b40270bd3883839010a55dd89
Oscar <oscarjaureguia@gmail.com>
2020-11-02 21:38:24 -0400

    chore: solution 10.1, initialize RN application using expo cli with a typescript template

---
+++ rate-repository-app/App.tsx
@@ -0,0 +1,24 @@
+import { StatusBar } from 'expo-status-bar';
+import React from 'react';
+import { StyleSheet, Text, View } from 'react-native';
+
+export default function App() {
+  return (
+    <View style={styles.container}>
+      <Text style={styles.text}>Hello World!</Text>
+      <StatusBar style="auto" />
+    </View>
+  );
+}
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    backgroundColor: '#000',
+    alignItems: 'center',
+    justifyContent: 'center',
+  },
+  text: {
+    color: '#fff',
+  }
+});
